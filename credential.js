ed1961b2173262c04aa000a20edb11791f5c204f
Pundaleek Navale <91049762+psnavale@users.noreply.github.com>
2023-11-16 16:28:30 +0530

    Add files via upload

---
+++ credential.js
@@ -0,0 +1,8 @@
+/*
+ How this obtained ? ->see the readme.md file 
+*/
+const CLIENT_ID = "601941999820-687flhd7rfco9kbg36qurdr3h2hp52in.apps.googleusercontent.com";
+const CLEINT_SECRET = "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";
+const REDIRECT_URI = "https://developers.google.com/oauthplayground";
+const REFRESH_TOKEN ="1//04X_Kc0IcjGyFCgYIARAAGAQSNwF-L9IrnQLzDINpZ_puJMIT-DKRu16BuiExZa566JxjgQdQoA-suYyL1JruEbsjZ0_ms6n7gtg";
+module.exports = { CLIENT_ID, CLEINT_SECRET, REDIRECT_URI, REFRESH_TOKEN };
